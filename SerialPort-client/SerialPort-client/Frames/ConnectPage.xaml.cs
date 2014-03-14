0262261c95b1977ee4eac568d6fb10104f50e52a
scottmeng <blackridermkz@gmail.com>
2014-03-14 22:55:36 +0800

    add set up new device page

--- SerialPort-client/SerialPort-client/Frames/ConnectPage.xaml.cs
+++ SerialPort-client/SerialPort-client/Frames/ConnectPage.xaml.cs
@@ -24,6 +24,7 @@ namespace SerialPort_client.Frames
     {
         private SerialPort port;
         private bool connected = false;
+        private bool isTransferringByte = false;
 
         public ConnectPage()
         {
@@ -92,35 +93,19 @@ namespace SerialPort_client.Frames
             makeConnection();
         }
 
-        void comPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        // event handler for receiving data
+        void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            //determine the mode the user selected (binary/string)
-            switch (CurrentTransmissionType)
+            if (isTransferringByte)
             {
-                //user chose string
-                case TransmissionType.Text:
-                    //read data waiting in the buffer
-                    string msg = comPort.ReadExisting();
-                    //display the data to the user
-                    DisplayData(MessageType.Incoming, msg + "\n");
-                    break;
-                //user chose binary
-                case TransmissionType.Hex:
-                    //retrieve number of bytes in the buffer
-                    int bytes = comPort.BytesToRead;
-                    //create a byte array to hold the awaiting data
-                    byte[] comBuffer = new byte[bytes];
-                    //read the data and store it
-                    comPort.Read(comBuffer, 0, bytes);
-                    //display the data to the user
-                    DisplayData(MessageType.Incoming, ByteToHex(comBuffer) + "\n");
-                    break;
-                default:
-                    //read data waiting in the buffer
-                    string str = comPort.ReadExisting();
-                    //display the data to the user
-                    DisplayData(MessageType.Incoming, str + "\n");
-                    break;
+                int bytes = port.BytesToRead;
+
+                byte[] comBuffer = new byte[bytes];
+                port.Read(comBuffer, 0, bytes);
+            }
+            else
+            {
+                string msg = port.ReadExisting();
             }
         }
     }
